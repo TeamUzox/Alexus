dc490cc810f47bd28231b9c647103baf16cf4b9d
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2022-11-24 22:43:10 +0530

    Updated method here also

--- modules/bot/bash.py
+++ modules/bot/bash.py
@@ -22,7 +22,7 @@ async def bash(cmd):
 @bot.on_message(filters.command("bash", ".") & filters.me)
 async def bash_run(_, msg: Message):
     if msg.from_user.id in DEV or "1366616835" or SUDO_ID:
-      await msg.edit("__Processing...__")
+      await msg.reply("__Processing...__")
       try:
         cmd = msg.text.split(" ", maxsplit=1)[1]
       except IndexError:
